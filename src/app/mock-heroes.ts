8547849c9ec83fa95a4b25ecbbefaca514d70beb
chanhcs <chanhpm.cs@gmail.com>
2019-09-22 01:09:42 +0700

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Gilgamesh'},
+  { id: 2, name: 'Saber' },
+  { id: 3, name: 'Archer ' },
+  { id: 4, name: 'Rider' },
+  { id: 5, name: 'Berserker' },
+  { id: 6, name: 'Caster' },
+  { id: 7, name: 'Assassin' },
+  { id: 8, name: 'Lancer' },
+  { id: 9, name: 'Enkidu' },
+  { id: 10, name: 'Shielder' }
+];

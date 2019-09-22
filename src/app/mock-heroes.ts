f7565b59a29890f0cc8834c441ef6fc0bc568aa2
chanhcs <chanhpm.cs@gmail.com>
2019-09-22 17:42:05 +0700

    Adding image

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 1, name: 'Gilgamesh'},
-  { id: 2, name: 'Saber' },
-  { id: 3, name: 'Archer ' },
-  { id: 4, name: 'Rider' },
-  { id: 5, name: 'Berserker' },
-  { id: 6, name: 'Caster' },
-  { id: 7, name: 'Assassin' },
-  { id: 8, name: 'Lancer' },
-  { id: 9, name: 'Enkidu' },
-  { id: 10, name: 'Shielder' }
+  { id: 1, name: 'Saber', imageUrl: 'https://cdn.shopify.com/s/files/1/0318/2649/products/519w9aN_2BuKL.jpeg?v=1425046667'},
+  { id: 2, name: 'Gilgamesh', imageUrl: 'https://i.pinimg.com/originals/49/c3/dd/49c3ddf5d16ff418d3717175a28b6a19.jpg'},
+  { id: 3, name: 'Archer ', imageUrl: 'https://www.picclickimg.com/d/l400/pict/332890215431_/F1705-Free-Mat-Bag-Fate-Stay-Night-Archer.jpg'},
+  { id: 4, name: 'Rider', imageUrl: 'https://i.pinimg.com/originals/46/ab/af/46abaf86fbb307dc929857d987b90a63.jpg' },
+  { id: 5, name: 'Berserker', imageUrl: 'https://static.zerochan.net/Berserker.%28Fate.stay.night%29.full.2466626.jpg' },
+  { id: 6, name: 'Caster', imageUrl: 'https://vignette.wikia.nocookie.net/villains/images/6/6b/Medea1.jpg/revision/latest?cb=20171122170326' },
+  { id: 7, name: 'Assassin', imageUrl: 'https://i.pinimg.com/originals/e9/1a/fa/e91afa3886975fb474d16bc5b8ae7852.jpg' },
+  { id: 8, name: 'Lancer', imageUrl: 'https://static.zerochan.net/Lancer.%28Fate.Prototype%29.full.1961014.jpg' },
+  { id: 9, name: 'Enkidu', imageUrl: 'https://vignette.wikia.nocookie.net/typemoon/images/7/77/LancerEnkiduStage3.jpg/revision/latest/scale-to-width-down/350?cb=20161216215910' },
+  { id: 10, name: 'Shielder', imageUrl: 'https://static.zerochan.net/Shielder.%28Fate.Grand.Order%29.full.1990858.jpg' }
 ];
